327dc9b80823249a40641716dc456eb2a90379b7
maomao <1449573893@qq.com>
2024-12-22 00:34:21 +0800

    安装了必需插件

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+/**
+ * axios
+ * pinia
+ * sass
+ * vue-router
+ * naive-ui
+ * wangeditor
+ */
+
 createApp(App).mount('#app')
